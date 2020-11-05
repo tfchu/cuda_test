87ba746c5511999e0b44e082ab7757698a8e0a35
tfchu <tfchu@hotmail.com>
2020-11-05 15:08:56 -0800

    update

--- c/thrust_sort.cu
+++ c/thrust_sort.cu
@@ -8,18 +8,28 @@
 
 int main(void)
 {
-  // generate 32M random numbers serially
-  thrust::host_vector<int> h_vec(32 << 20);
-  std::generate(h_vec.begin(), h_vec.end(), rand);
+    // generate 32M random numbers serially (2^20 = 1M)
+    // thrust::host_vector<int> h_vec(32 << 20);
+    // generate 1M random numbers serially (2^20 = 1M)
+    thrust::host_vector<int> h_vec(1 << 20);
+    std::generate(h_vec.begin(), h_vec.end(), rand);
 
-  // transfer data to the device
-  thrust::device_vector<int> d_vec = h_vec;
+    // print d_vec, size = d_vec.size()
+    for(int i = 0; i < 50; i++)
+        std:cout << "h_dev[" << i << "] = " << h_vec[i] << std.endl;
 
-  // sort data on the device (846M keys per second on GeForce GTX 480)
-  thrust::sort(d_vec.begin(), d_vec.end());
+    // transfer data to the device
+    thrust::device_vector<int> d_vec = h_vec;
 
-  // transfer data back to host
-  thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+     // sort data on the device (846M keys per second on GeForce GTX 480)
+    thrust::sort(d_vec.begin(), d_vec.end());
+
+    // transfer data back to host
+    thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+
+    // print d_vec, size = d_vec.size()
+    for(int i = 0; i < 50; i++)
+        std:cout << "h_dev[" << i << "] = " << h_vec[i] << std.endl;
 
   return 0;
 }
