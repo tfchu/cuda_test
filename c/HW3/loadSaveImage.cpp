420875fb615528aef9832f977309d3f4398bd822
tfchu <tfchu@hotmail.com>
2020-11-05 19:47:39 -0800

    update

--- c/HW3/loadSaveImage.cpp
+++ c/HW3/loadSaveImage.cpp
@@ -13,7 +13,7 @@ void loadImageHDR(const std::string &filename,
                   float **imagePtr,
                   size_t *numRows, size_t *numCols)
 {
-  cv::Mat image = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR | CV_LOAD_IMAGE_ANYDEPTH);
+  cv::Mat image = cv::imread(filename.c_str(), IMREAD_COLOR | IMREAD_ANYDEPTH); // CV_LOAD_IMAGE_COLOR, CV_LOAD_IMAGE_ANYDEPTH
   if (image.empty()) {
     std::cerr << "Couldn't open file: " << filename << std::endl;
     exit(1);
@@ -43,7 +43,7 @@ void loadImageRGBA(const std::string &filename,
                    uchar4 **imagePtr,
                    size_t *numRows, size_t *numCols)
 {
-  cv::Mat image = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR);
+  cv::Mat image = cv::imread(filename.c_str(), IMREAD_COLOR);    // CV_LOAD_IMAGE_COLOR
   if (image.empty()) {
     std::cerr << "Couldn't open file: " << filename << std::endl;
     exit(1);
@@ -60,7 +60,7 @@ void loadImageRGBA(const std::string &filename,
   }
 
   cv::Mat imageRGBA;
-  cv::cvtColor(image, imageRGBA, CV_BGR2RGBA);
+  cv::cvtColor(image, imageRGBA, cv::COLOR_BGR2RGBA);    // CV_BGR2RGBA
 
   *imagePtr = new uchar4[image.rows * image.cols];
 
@@ -85,7 +85,7 @@ void saveImageRGBA(const uchar4* const image,
   sizes[1] = numCols;
   cv::Mat imageRGBA(2, sizes, CV_8UC4, (void *)image);
   cv::Mat imageOutputBGR;
-  cv::cvtColor(imageRGBA, imageOutputBGR, cv::COLOR_RGBA2BGR);
+  cv::cvtColor(imageRGBA, imageOutputBGR, cv::COLOR_RGBA2BGR);  // CV_RGBA2BGR
   //output the image
   cv::imwrite(output_file.c_str(), imageOutputBGR);
 }
