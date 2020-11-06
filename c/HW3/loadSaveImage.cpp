e95ed6312693b873796b33d754090e61ca299575
tfchu <tfchu@hotmail.com>
2020-11-05 19:54:14 -0800

    update

--- c/HW3/loadSaveImage.cpp
+++ c/HW3/loadSaveImage.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imcodecs.hpp>
 #include <vector>
 #include "cuda_runtime.h"
 
